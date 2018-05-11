e0e6d50174ccdf50f8c3c3af230a4c16a950725b
=Mahsa <mahsa.gol89@gmail.com>
2018-05-12 00:37:55 +0430

    'day1'

--- day1.py
+++ day1.py
@@ -1,25 +1,23 @@
-# Declare second integer, double, and String variables.
-
-# Read and save an integer, double, and String to your variables.
-
-# Print the sum of both integer variables on a new line.
-
-# Print the sum of the double variables on a new line.
-
-# Concatenate and print the String variables on a new line
-
-# The 's' variable above should be printed first.
 i = 4
 d = 4.0
 s = 'HackerRank '
+# Declare second integer, double, and String variables.
+# Don't need to explicitly declare variables in Python!
 
-a = int(input(2))
-b = float(input(2.0))
-c = str(input('salam'))
+# Read and save an integer, double, and String to your variables.
+a = int(input())
+b = float(input())
+c = str(input())
 
-print (str(a+i))
-print (str(b+d))
-print (str(c+s))
+# Print the sum of both integer variables on a new line.
+print (i + a)
+
+# Print the sum of the double variables on a new line.
+print (format(d + b, '.1f'))
+
+# Concatenate and print the String variables on a new line
+# The 's' variable above should be printed first.
+print (s + c)
 
 
 
