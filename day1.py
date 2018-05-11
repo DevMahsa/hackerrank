7783d60a5e5d88a9596c85b916a9fe10fc93fa10
=Mahsa <mahsa.gol89@gmail.com>
2018-05-12 00:33:10 +0430

    day0

---
+++ day1.py
@@ -0,0 +1,26 @@
+# Declare second integer, double, and String variables.
+
+# Read and save an integer, double, and String to your variables.
+
+# Print the sum of both integer variables on a new line.
+
+# Print the sum of the double variables on a new line.
+
+# Concatenate and print the String variables on a new line
+
+# The 's' variable above should be printed first.
+i = 4
+d = 4.0
+s = 'HackerRank '
+
+a = int(input(2))
+b = float(input(2.0))
+c = str(input('salam'))
+
+print (str(a+i))
+print (str(b+d))
+print (str(c+s))
+
+
+
+
